8eea068fabd27c4a6b5fdf755fda1af3873d03fa
Haseop Lee <haservi@naver.com>
2022-10-12 21:37:37 +0900

    chore: 선언 순서 변경

--- src/main/java/baseball/common/ConstRegex.java
+++ src/main/java/baseball/common/ConstRegex.java
@@ -2,7 +2,7 @@ package baseball.common;
 
 public class ConstRegex {
 
-    public final static String ONE_TO_NINE = "[1-9]+";
+    public static final String ONE_TO_NINE = "[1-9]+";
 
 
 }
--- src/main/java/baseball/common/ConstValue.java
+++ src/main/java/baseball/common/ConstValue.java
@@ -2,8 +2,8 @@ package baseball.common;
 
 public class ConstValue {
 
-    public final static int MIN_NUMBER = 1;
-    public final static int MAX_NUMBER = 9;
-    public final static int TOTAL_ROUND = 3;
-    public final static int INPUT_LENGTH = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final int TOTAL_ROUND = 3;
+    public static final int INPUT_LENGTH = 3;
 }
